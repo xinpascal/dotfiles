ec48c8848ee8e1caa22e3ae81120902e372f02f3
Xin Ouyang <xinpascal@gmail.com>
2021-09-11 02:56:23 +0000

    fix firefox theme install issue.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -9,3 +9,16 @@ done
 
 dotfiles=".Xclients"
 cp -rf $dotfiles ~/
+
+for d in ~/.mozilla/firefox/*default*; do
+    if [[ -f "${d}/prefs.js" ]]; then
+      rm -rf                                                                                  "${d}/chrome"
+      ln -sf ../firefox-themes                                                                "${d}/chrome"
+      echo "user_pref(\"toolkit.legacyUserProfileCustomizations.stylesheets\", true);" >>     "${d}/prefs.js"
+      echo "user_pref(\"browser.tabs.drawInTitlebar\", true);" >>                             "${d}/prefs.js"
+      echo "user_pref(\"browser.uidensity\", 0);" >>                                          "${d}/prefs.js"
+      echo "user_pref(\"layers.acceleration.force-enabled\", true);" >>                       "${d}/prefs.js"
+      echo "user_pref(\"mozilla.widget.use-argb-visuals\", true);" >>                         "${d}/prefs.js"
+    fi
+done
+
